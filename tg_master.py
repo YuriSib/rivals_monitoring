e7df4cab21d28d15f652917e8b745fcfb5b4ee57
YuriSib <sib_94@vk.com>
2023-12-06 20:27:47 +0300

    Завершил работу над логикой проекта, испытания прошли успешно. Осталось поставить отловы ошибок и сделать вывод сообщения об ошибке мне в ТГ. В целом уже можно привязывать програму к ТГ заказчика.

--- tg_master.py
+++ tg_master.py
@@ -12,7 +12,7 @@ def test(message_):
 
 def check_message():
     # bot.send_message(-1002049731505, f'Проверка')
-    bot.send_message(-2056827090, f'Проверка')
+    bot.send_message(-1002056827090, f'Проверка')
 
 
 def send_message(key, event):
